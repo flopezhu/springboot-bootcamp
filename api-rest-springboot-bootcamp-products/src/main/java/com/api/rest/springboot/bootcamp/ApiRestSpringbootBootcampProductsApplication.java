fd5db57a3c9e45253fd395cd8cf72a4cc551c213
flopezhu <franclin.lopezhuayanay@nttdata.com>
2022-05-15 01:51:18 -0500

    add new dependency bootstrap

--- api-rest-springboot-bootcamp-products/src/main/java/com/api/rest/springboot/bootcamp/ApiRestSpringbootBootcampProductsApplication.java
+++ api-rest-springboot-bootcamp-products/src/main/java/com/api/rest/springboot/bootcamp/ApiRestSpringbootBootcampProductsApplication.java
@@ -1,20 +1,12 @@
 package com.api.rest.springboot.bootcamp;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiRestSpringbootBootcampProductsApplication {
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestSpringbootBootcampProductsApplication.class, args);
 	}
-
 }
